90e43682f0fafa6a484f25710a7c31959e2f3fd2
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2023-10-26 17:10:51 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -4,16 +4,28 @@ const titleBox = document.getElementById('essay');
 const essay = document.getElementById('essay-content')
 
 console.log(essays.length)
-console.log(essays[7].title)
 
-const theEssay = essays[7];
+const titles = []
+const thisEssayTitle = 'fresh water'
+
+for (let i = 0; i < essays.length; i++) {
+    titles.push(essays[i].title)
+}
+
+console.log(titles)
+
+const index = titles.indexOf(thisEssayTitle)
+
+console.log(index)
+
+const theEssay = essays[index];
 
 
 titleBox.appendChild(document.createElement('h3')).innerHTML = theEssay.title;
 titleBox.appendChild(document.createElement('p')).innerHTML = theEssay.prompt;
 titleBox.appendChild(document.createElement('p')).innerHTML = theEssay.question;
 
-console.log(theEssay.essay.length)
+console.log(theEssay.essay.length + ' paragraphs')
 
 for (let i = 0; i < theEssay.essay.length; i++) {
     essay.appendChild(document.createElement('p')).innerHTML = theEssay.essay[i]
